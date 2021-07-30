22df5845938e50d76c7ae7a6eb65ee5a99aa26aa
KostenokLisa <kostenok.en@phystech.edu>
2021-07-30 11:58:19 +0300

    improve codestyle

--- swag.py
+++ swag.py
@@ -6,42 +6,43 @@ import numpy as np
 import copy
 import itertools
 
+
 def param_to_vector(parameters):
-  res = []
-  for param in parameters:
-      res.append(param.view(-1))
-  res_tensor = torch.cat(res)   
-  res_tensor = res_tensor.cpu() 
-  return res_tensor.detach().numpy()
+    res = []
+    for param in parameters:
+        res.append(param.view(-1))
+    res_tensor = torch.cat(res)
+    res_tensor = res_tensor.cpu()
+    return res_tensor.detach().numpy()
+
 
 def train_SWAG(net_fn, log_posterior_fn, trainset, T=1000, batch_size=100, c=50, lr=0.001, K=10):
-    
-  momentum_decay = 0.9
-  optimizer = optim.SGD(net_fn.parameters(), lr=lr, momentum=momentum_decay)
+    momentum_decay = 0.9
+    optimizer = optim.SGD(net_fn.parameters(), lr=lr, momentum=momentum_decay)
 
-  SWAG_loader = itertools.islice(itertools.cycle(DataLoader(trainset, batch_size=batch_size, shuffle=True)), 0, T)
+    SWAG_loader = itertools.islice(itertools.cycle(DataLoader(trainset, batch_size=batch_size, shuffle=True)), 0, T)
 
-  teta = param_to_vector(net_fn.parameters())
-  teta_sqr = teta*teta
-  D = []
+    teta = param_to_vector(net_fn.parameters())
+    teta_sqr = teta * teta
+    D = []
 
-  for i, data in enumerate(SWAG_loader): 
-    optimizer.zero_grad()
-    model_state_dict = copy.deepcopy(net_fn.state_dict()) 
+    for i, data in enumerate(SWAG_loader):
+        optimizer.zero_grad()
+        model_state_dict = copy.deepcopy(net_fn.state_dict())
 
-    loss = - log_posterior_fn(model_state_dict, data)
-    loss.backward()
-    optimizer.step()
+        loss = - log_posterior_fn(model_state_dict, data)
+        loss.backward()
+        optimizer.step()
 
-    if (i+1)%c == 0:
-      n = (i+1)/c
-      new_teta = param_to_vector(net_fn.parameters())
-      teta = (n*teta + new_teta)/(n+1)
-      teta_sqr = (n*teta_sqr + new_teta*new_teta)/(n+1)
+        if (i + 1) % c == 0:
+            n = (i + 1) / c
+            new_teta = param_to_vector(net_fn.parameters())
+            teta = (n * teta + new_teta) / (n + 1)
+            teta_sqr = (n * teta_sqr + new_teta * new_teta) / (n + 1)
 
-      if len(D) > K:
-        del D[0]
-      D.append(new_teta - teta)
+            if len(D) > K:
+                del D[0]
+            D.append(new_teta - teta)
 
-  D = np.array(D)
-  return teta, teta_sqr - teta*teta, D
+    D = np.array(D)
+    return teta, teta_sqr - teta * teta, D
